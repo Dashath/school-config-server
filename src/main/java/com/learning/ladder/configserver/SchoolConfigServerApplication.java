193cfd0109e1f74e875e5ea634f5bff7661da4b7
dashrath <dashrath.rajput@gmail.com>
2021-02-07 12:55:14 +0530

    initial commit

---
+++ src/main/java/com/learning/ladder/configserver/SchoolConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.learning.ladder.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolConfigServerApplication.class, args);
+	}
+
+}
